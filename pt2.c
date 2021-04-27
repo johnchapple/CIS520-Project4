fcf90ad35746b5b4c0d9e40785a9d8505b53172e
Grant <Grantemc2@att.net>
2021-04-27 16:01:40 -0500

    add first MPI test

---
+++ pt2.c
@@ -0,0 +1,117 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//#define NUM_THREADS 4
+int NUM_THREADS;
+
+#define ARRAY_SIZE 1000
+#define STRING_SIZE 2001
+
+char line_array[ARRAY_SIZE][STRING_SIZE];
+float line_avg[ARRAY_SIZE];			// count of individual characters
+float local_char_count[ALPHABET_SIZE];
+
+void init_arrays()
+{
+  int i, j, err;
+  FILE *fd;
+
+   fd = fopen( "/homes/dan/625/wiki_dump.txt", "r" );
+   for ( i = 0; i < ARRAY_SIZE; i++ )  {
+      err = fscanf( fd, "%[^\n]\n", line_array[i]);
+      if( err == EOF ) break;
+   }
+
+  for ( i = 0; i < ARRAY_SIZE; i++ ) {
+  	line_avg[i] = 0.0;
+  }
+}
+
+float find_avg(char* line, int nchars) {
+   int i, j;
+   float sum = 0;
+
+   for ( i = 0; i < nchars; i++ ) {
+      sum += ((int) line[i]);
+   }
+
+   if (nchars > 0)
+        return sum / (float) nchars;
+   else
+        return 0.0;
+}
+
+void *count_array(void *rank)
+{
+  char theChar;
+  int i, j, charLoc;
+  int myID =  *((int*) rank);
+
+  int startPos = ((long) myID) * (ARRAY_SIZE / NUM_THREADS);
+  int endPos = startPos + (ARRAY_SIZE / NUM_THREADS);
+
+  printf("myID = %d startPos = %d endPos = %d \n", myID, startPos, endPos); fflush(stdout);
+
+    // init local count array
+  for ( i = 0; i < ARRAY_SIZE; i++ ) {
+  	local_line_avg[i] = 0.0;
+  }
+
+    // count up our section of the global array
+  for ( i = startPos; i < endPos; i++) {
+     local_line_avg[i]=find_avg(line_array[i], strlen(line_array[i]));
+  }
+}
+
+void print_results(float the_line_avg[])
+{
+  int i,j, total = 0;
+
+  					// then print out the totals
+  for ( i = 0; i < ARRAY_SIZE; i++ ) {
+	printf("%d: %.1f\n", i, the_line_avg[i]);
+  }
+}
+}
+
+main(int argc, char* argv[]) 
+{
+	int i, rc;
+	int numtasks, rank;
+	MPI_Status Status;
+
+
+	rc = MPI_Init(&argc,&argv);
+	if (rc != MPI_SUCCESS) {
+	  printf ("Error starting MPI program. Terminating.\n");
+          MPI_Abort(MPI_COMM_WORLD, rc);
+        }
+
+        MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
+        MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+
+	NUM_THREADS = numtasks;
+	printf("size = %d rank = %d\n", numtasks, rank);
+	fflush(stdout);
+
+	if ( rank == 0 ) {
+		init_arrays();
+	}
+	MPI_Bcast(line_array, ARRAY_SIZE * STRING_SIZE, MPI_CHAR, 0, MPI_COMM_WORLD);
+		
+	count_array(&rank);
+
+    //MPI_Reduce(local_char_count, char_counts, ALPHABET_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	MPI_Reduce(local_line_avg, line_avg, ARRAY_SIZE, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+
+	if ( rank == 0 ) {
+		print_results();
+	}
+
+	MPI_Finalize();
+	return 0;
+}
+
