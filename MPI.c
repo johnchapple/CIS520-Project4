e9f99b93c560622aca6140d2e37bcb8c47b591c2
Grant <Grantemc2@att.net>
2021-05-04 10:50:35 -0500

    changed mpi.c

--- MPI.c
+++ MPI.c
@@ -103,9 +103,10 @@ main(int argc, char* argv[])
 	fflush(stdout);
 
 	if ( rank == 0 ) {
-    printf("###%d###\n", strtol(argv[0],NULL, 10));	
-    printf("###%d###\n", strtol(argv[1],NULL, 10));	
-    printf("###%d###\n", strtol(argv[2],NULL, 10));	
+    //mpirun -np 2 MPI 2
+    printf("###%d###\n", strtol(argv[0],NULL, 10));	//0
+    printf("###%d###\n", strtol(argv[1],NULL, 10));	//2
+    //printf("###%d###\n", strtol(argv[2],NULL, 10));	//seg fault
     printf("###%d###\n", strtol(argv[3],NULL, 10));	
     printf("###%d###\n", strtol(argv[4],NULL, 10));	
     cpu_num = strtol(argv[4], NULL, 10);
