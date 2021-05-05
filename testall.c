2c75bbcfa0c3f83cb40a965dd1a2bd7d3a385eae
GRANT Nicholas CLOTHIER <grantxc5@selene.beocat.ksu.edu>
2021-05-04 20:57:00 -0500

    added slurm files

--- testall.c
+++ testall.c
@@ -12,10 +12,10 @@ int main()
     help = system(command);
     char g2[100] = 
         "sbatch sb.OpenMP-1; sbatch sb.OpenMP-2; sbatch sb.OpenMP-3; sbatch sb.OpenMP-4;";
-    //help = system(g2);
+    help = system(g2);
     char g3[100] = 
         "sbatch sb.Pthreads-1; sbatch sb.Pthreads-2; sbatch sb.Pthreads-3; sbatch sb.Pthreads-4;";
-    //help = system(g3);
+    help = system(g3);
     return 0;
 }
 
